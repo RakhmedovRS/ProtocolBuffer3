ee85adaf02e67ca6dc1b30815a5534c93e8e4e54
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2022-03-24 20:12:47 -0700

    set up project for section 6

---
+++ src/main/java/com/github/rakhmedovrs/protocolbuffer3/SimpleMain.java
@@ -0,0 +1,13 @@
+package com.github.rakhmedovrs.protocolbuffer3;
+
+/**
+ * @author RakhmedovRS
+ * @created 3/24/2022
+ */
+public class SimpleMain
+{
+	public static void main(String[] args)
+	{
+		System.out.println();
+	}
+}
